4cf4daab9c104222e9447ca1da285209b392b762
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-06-07 00:18:19 -0300

    Alterações Linux

--- linux/startup.sh
+++ linux/startup.sh
@@ -1,20 +1,32 @@
-# echo 'update system'
+echo 'update system'
 sudo apt-get update
 clear
 
-# echo 'Installing Snap'
+echo 'Installing Snap'
 sudo apt install snapd -y
 
-# echo 'installing curl'
+echo 'installing curl'
 sudo apt install curl -y
 clear
 
- echo 'installing fix-broken'
- sudo apt --fix-broken install -y
+echo 'installing fix-broken'
+sudo apt --fix-broken install -y
 
+echo 'installing chrome'
+sudo apt install -f
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo rm -r google-chrome-stable_current_amd64.deb
+sudo apt-get update
+clear
 
-# echo 'installing vscode'
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+echo 'installing fira code'
+sudo apt install fonts-firacode -y
+sudo apt-get update
+clear
+
+echo 'installing vscode'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get install apt-transport-https -y
@@ -22,63 +34,39 @@ sudo apt-get update
 sudo apt-get install code -y
 clear
 
-
-# echo 'installing git'
+echo 'installing git'
 sudo apt-get install git -y
 
-# echo 'config git'
-echo 'whats your user.name'
-read git_user_name
+echo 'config git'
 git config --global user.name "Paulo Barbosa"
-echo 'whats your user.email'
-read git_user_email
 git config --global user.email "paulohjbarbosa@gmail.com"
 git config --global core.editor "code --wait"
+sudo apt-get update
 clear
 
-# echo 'installing chrome'
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo rm -r google-chrome-stable_current_amd64.deb
-clear
+echo 'installing Nodejs'
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+nodejs -v
+npm -v
+sudo apt-get update
 
-# echo 'installing hyper'
+echo 'installing hyper'
+sudo apt --fix-broken install
 wget -O hyper.deb https://releases.hyper.is/download/deb
 sudo dpkg -i hyper.deb
 sudo rm -r hyper.deb
+sudo apt-get update
 clear
 
-
-echo 'installing nvm'
-sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
-
-export NVM_DIR="$HOME/.nvm" && (
-git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-cd "$NVM_DIR"
-git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-) && \. "$NVM_DIR/nvm.sh"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-source ~/.zshrc
-nvm --version
-nvm install 12
-nvm alias default 12
-node --version
-npm --version
-
-# echo 'installing fira code'
-sudo apt install fonts-firacode -y
-clear
-
-# echo 'installing spotify'
+echo 'installing spotify'
+sudo apt install -f
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 clear
 
-# echo 'installing extensions vscode'
+echo 'installing extensions vscode'
 code --install-extension aaron-bond.better-comments
 code --install-extension abusaidm.html-snippets
 code --install-extension adrianwilczynski.add-reference
@@ -133,27 +121,32 @@ code --install-extension vscode-icons-team.vscode-icons
 code --install-extension wayou.vscode-todo-highlight
 clear
 
-# echo 'installing zsh'
+echo 'installing zsh'
 sudo apt-get install zsh -y
+sudo apt-get update
 clear
 
-#echo 'installing dbaever'
+echo 'installing dbaever'
 sudo snap install dbeaver-ce
+sudo apt-get update
 clear
 
-# echo 'installing Sqlite Browser'
+echo 'installing Sqlite Browser'
+sudo apt install -f
 sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser
 sudo apt-get update
 sudo apt-get install sqlitebrowser -y
 clear
 
-# echo 'installing WPS'
+echo 'installing WPS'
+sudo apt-get update
 sudo apt-get install wps-office-all-lang-no-internet
 clear
 
-# echo 'installing Obs Studio'
+echo 'installing Obs Studio'
+sudo apt install -f
 sudo apt install ffmpeg -y
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install obs-studio -y
-
+sudo apt-get update
